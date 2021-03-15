51b968bf3f43905f36f642ef2938102b2070aba5
drykxs <42818396+drykxs@users.noreply.github.com>
2021-03-15 14:53:08 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 sudo openstack tripleo container image prepare --environment-file /home/stack/templates/containers-prepare-parameters.yaml
 sudo openstack tripleo deploy \
 	--templates \
-	--local-ip=192.168.12.2/24 \
+	--local-ip=192.168.25.2/24 \
 	-e /usr/share/openstack-tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml \
 	-r /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
 	-e /home/stack/templates/containers-prepare-parameters.yaml \

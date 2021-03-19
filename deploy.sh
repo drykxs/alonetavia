373943f11f6a0b81961b388df096c479de432aaf
drykxs <42818396+drykxs@users.noreply.github.com>
2021-03-19 15:39:20 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 sudo openstack tripleo deploy \
-	--templates \
+	--templates /usr/share/openstack-tripleo-heat-templates \
 	--local-ip=192.168.25.2/24 \
+	-e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+	-e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
 	-e /usr/share/openstack-tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml \
-        -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
 	-r /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
 	-e /home/stack/templates/containers-prepare-parameters.yaml \
 	-e /home/stack/templates/standalone-params.yaml \
 	--output-dir /home/stack/installog \
+	--standalone \

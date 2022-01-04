201abfff4a45736d79ddeaf8f7268ddbf8ea6098
drykxs <42818396+drykxs@users.noreply.github.com>
2022-01-04 00:15:41 -0300

    Update pre-install.sh
    This is for repos and packages pre osp install

--- pre-install.sh
+++ pre-install.sh
@@ -1,12 +1,6 @@
 sudo subscription-manager release --set=8.4;
 sudo subscription-manager repos --disable=*; 
-sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-eus-rpms \
---enable=rhel-8-for-x86_64-appstream-eus-rpms \
---enable=rhel-8-for-x86_64-highavailability-eus-rpms \
---enable=ansible-2.9-for-rhel-8-x86_64-rpms \
---enable=openstack-16.1-for-rhel-8-x86_64-rpms \
---enable=fast-datapath-for-rhel-8-x86_64-rpms \
---enable=advanced-virt-for-rhel-8-x86_64-rpms \ 
+sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-eus-rpms --enable=rhel-8-for-x86_64-appstream-eus-rpms  --enable=rhel-8-for-x86_64-highavailability-eus-rpms --enable=ansible-2.9-for-rhel-8-x86_64-rpms --enable=openstack-16.1-for-rhel-8-x86_64-rpms --enable=fast-datapath-for-rhel-8-x86_64-rpms --enable=advanced-virt-for-rhel-8-x86_64-rpms;
 sudo dnf module disable -y container-tools:rhel8;
 sudo dnf module enable -y container-tools:3.0;
 sudo dnf update -y; 
